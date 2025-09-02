38bce9db83cdfce07ab9f75f61221dfac60dcd38
RybazPuszczy <75949266+RybazPuszczy@users.noreply.github.com>
2025-09-02 16:33:15 +0200

    Let's start with a few devex improvements

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import { defineConfig } from "eslint/config";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default defineConfig(
+  {
+    ignores: ["./*.js", "./*.mjs"],
+  },
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+);
